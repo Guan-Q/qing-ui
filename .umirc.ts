71f40fc0811cd8180f500141727b881631b2c134
Guan-Q <2175933767@qq.com>
2025-12-01 11:51:45 +0800

    fix:部署页面显示

--- .umirc.ts
+++ .umirc.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from 'dumi';
 
-let base = '/qing-ui';
-let publicPath = '/qing-ui/';
-
-if (process.env.SITE_BUILD_ENV === 'PREVIEW') {
-  base = '';
-  publicPath = '';
-}
+const isPreview = process.env.SITE_BUILD_ENV === 'PREVIEW';
+const repoName = 'qing-ui';
 
 export default defineConfig({
-  title: 'Qing UI', // 站点名称
+  title: 'Qing UI',
   mode: 'site',
-  outputPath: 'dist', // 输出文件夹
-  exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
-  dynamicImport: {}, // 拆包 站点过大时可以优化首屏加载速度
+  outputPath: 'dist',
+  exportStatic: {},
+  dynamicImport: {},
+  base: isPreview ? '/' : `/${repoName}`,
+  publicPath: isPreview ? '/' : `/${repoName}/`,
 });
